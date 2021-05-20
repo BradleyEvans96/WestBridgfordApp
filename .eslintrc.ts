7fe84334bce4b55050b746253d2f7f54a922b0d7
JoshPavBjss <josh.paveley@bjss.com>
2021-05-20 09:29:52 +0100

    Added linting

---
+++ .eslintrc.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: [ 
+    '@react-native-community', 
+    'airbnb-typescript', 
+    'prettier'
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+};
